764341604b9a6f0704e71ed753828e08e9761902
Matt McCloskey <matthew.lawrence.mccloskey@gmail.com>
2018-04-04 22:20:16 -0400

    began incorporating hristos work into my layout with 3 buttons

---
+++ assets/scripts/finance.js
@@ -0,0 +1,10 @@
+$("#homeButtons").on("click", "#financeBtn", function(){
+    $("#homeButtons").empty();
+    $("#homeButtons").append(homeBtn+'<button type="button" class="btn btn-success" id="stocksBtn">STOCKS</button> <br> <button type="button" class="btn btn-primary" id="cryptoBtn">CRYPTO</button> <br> <button type="button" class="btn btn-danger" id="financeNewsBtn">NEWS</button>');    
+});
+
+$("#homeButtons").on("click", "#stocksBtn", function(){
+    $("#homeButtons").empty();
+    $("#homeButtons").prepend(homeBtn);
+    $("#map").append('<iframe src="https://investingwidgets.com/leading-stocks?theme=darkTheme" width="100%" height="380" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">')                                   
+});
